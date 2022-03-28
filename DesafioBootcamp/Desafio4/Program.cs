bec3b3c1ea0786edcd5878416f9d1b91f7956ab3
GabrielMenezes98 <gabrielmenezes6916@gmail.com>
2022-03-28 16:18:00 -0300

    Changes DIO

---
+++ DesafioBootcamp/Desafio4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Classe {
+    static void Main() {
+        string[] line = Console.ReadLine().Split(" ");
+        
+        double H = double.Parse(line[0]);
+        double P = double.Parse(line[1]);
+        double MEDIA = H / P;
+        Console.WriteLine(Math.Round(MEDIA, 2).ToString("N2"));   
+    }   
+}
